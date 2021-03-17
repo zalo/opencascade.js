8715bc572482869b33fc550f01eae579bbe448b2
Johnathon Selstad <makeshifted@gmail.com>
2021-03-16 18:08:23 -0700

    Rename this so VS Code can automatically discover it

--- postTypes.js
+++ postTypes.js
@@ -1,3 +1,3 @@
 const fs = require("fs");
 const file = fs.readFileSync("types/opencascade.d.ts", "utf8").replace(/UnderscoresCanCauseProblems/g, "_");
-fs.writeFileSync("dist/opencascade.d.ts", file);
+fs.writeFileSync("dist/opencascade.wasm.d.ts", file);
